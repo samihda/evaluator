e7bc73e293132fe4d7d7021a67cb37f60ca3fa1b
samihda <samihda@users.noreply.github.com>
2019-01-14 22:50:48 +0100

    Remove export list

--- lib/predicates.scm
+++ lib/predicates.scm
@@ -1,66 +1,49 @@
-(define-module (predicates)
-  #: export (tagged-list?
-             self-eval?
-             var?
-             quoted?
-             assignment?
-             definition?
-             if?
-             lambda?
-             begin?
-             cond?
-             application?
-             compound-proc?
-             no-operands?
-             last-exp?
-             cond-else-clause?
-             primitive-proc?
-             true?))
+(define-module (predicates))
 
 (use-modules ((selectors) #:select (cond-predicate)))
 
-(define (tagged-list? exp tag)
+(define-public (tagged-list? exp tag)
   (if (pair? exp)
       (eq? (car exp) tag)
       #f))
 
-(define (self-eval? exp)
+(define-public (self-eval? exp)
   (cond ((number? exp) #t)
         ((string? exp) #t)
         (else #f)))
 
-(define (var? exp) (symbol? exp))
+(define-public (var? exp) (symbol? exp))
 
-(define (quoted? exp) (tagged-list? exp 'quote))
+(define-public (quoted? exp) (tagged-list? exp 'quote))
 
-(define (assignment? exp)
+(define-public (assignment? exp)
   (tagged-list? exp 'set!))
 
-(define (definition? exp)
+(define-public (definition? exp)
   (tagged-list? exp 'define))
 
-(define (if? exp) (tagged-list? exp 'if))
+(define-public (if? exp) (tagged-list? exp 'if))
 
-(define (lambda? exp) (tagged-list? exp 'lambda))
+(define-public (lambda? exp) (tagged-list? exp 'lambda))
 
-(define (begin? exp) (tagged-list? exp 'begin))
+(define-public (begin? exp) (tagged-list? exp 'begin))
 
-(define (cond? exp) (tagged-list? exp 'cond))
+(define-public (cond? exp) (tagged-list? exp 'cond))
 
-(define (application? exp) (pair? exp))
+(define-public (application? exp) (pair? exp))
 
-(define (compound-proc? p)
+(define-public (compound-proc? p)
   (tagged-list? p 'procedure))
 
-(define (no-operands? ops) (null? ops))
+(define-public (no-operands? ops) (null? ops))
 
-(define (last-exp? seq) (null? (cdr seq)))
+(define-public (last-exp? seq) (null? (cdr seq)))
 
-(define (cond-else-clause? clause)
+(define-public (cond-else-clause? clause)
   (eq? (cond-predicate clause) 'else))
 
-(define (primitive-proc? proc)
+(define-public (primitive-proc? proc)
   (tagged-list? proc 'primitive))
 
-(define (true? x) (not (eq? x #f)))
-;; (define (false? x) (eq? x #f))
+(define-public (true? x) (not (eq? x #f)))
+;; (define-public (false? x) (eq? x #f))
