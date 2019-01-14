b6290a43edfde5b039d15b0f60147b677ecc231c
samihda <samihda@users.noreply.github.com>
2019-01-14 23:27:02 +0100

    Rearrange

--- metacircular.scm
+++ metacircular.scm
@@ -8,7 +8,65 @@
 (use-modules ((env) #:prefix env:))
 (use-modules ((io) #:prefix io:))
 
-(define apply-in-guile apply)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define original-apply apply)
+
+(define (apply-primitive-proc proc args)
+  (original-apply
+   (sel:primitive-implementation proc) args))
+
+(define (apply proc args)
+  (cond ((primitive-proc? proc)
+         (apply-primitive-proc proc args))
+        ((compound-proc? proc)
+         (eval-seq
+          (sel:proc-body proc)
+          (env:extend (sel:proc-params proc) args (sel:proc-env proc))))
+        (else
+         (error "Unknown procedure type: APPLY" proc))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (eval-if exp env)
+  (if (true? (eval (sel:if-p exp) env))
+      (eval (sel:if-then exp) env)
+      (eval (sel:if-else exp) env)))
+
+(define (eval-seq exps env)
+  (cond ((last-exp? exps) (eval (sel:first-exp exps) env))
+        (else (eval (sel:first-exp exps) env)
+              (eval-seq (sel:rest-exps exps) env))))
+
+(define (make-procedure params body env)
+  (list 'procedure params body env))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+(define (make-begin seq) (cons 'begin seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (sel:first-exp seq))
+        (else (make-begin seq))))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp
+                 (sel:cond-actions first))
+                (error "ELSE clause is not last: COND->IF" clauses))
+            (make-if (sel:cond-predicate first)
+                     (sequence->exp (sel:cond-actions first))
+                     (expand-clauses rest))))))
+
+(define (cond->if exp)
+  (expand-clauses (sel:cond-clauses exp)))
 
 (define (list-of-values exps env)
   (if (no-operands? exps)
@@ -33,60 +91,7 @@
         (else
          (error "Unknown expression type -- EVAL" exp))))
 
-(define (apply proc args)
-  (cond ((primitive-proc? proc)
-         (apply-primitive-proc proc args))
-        ((compound-proc? proc)
-         (eval-seq
-          (sel:proc-body proc)
-          (env:extend (sel:proc-params proc) args (sel:proc-env proc))))
-        (else
-         (error "Unknown procedure type: APPLY" proc))))
-
-(define (eval-if exp env)
-  (if (true? (eval (sel:if-p exp) env))
-      (eval (sel:if-then exp) env)
-      (eval (sel:if-else exp) env)))
-
-(define (eval-seq exps env)
-  (cond ((last-exp? exps) (eval (sel:first-exp exps) env))
-        (else (eval (sel:first-exp exps) env)
-              (eval-seq (sel:rest-exps exps) env))))
-
-(define (make-begin seq) (cons 'begin seq))
-
-
-(define (cond->if exp)
-  (expand-clauses (sel:cond-clauses exp)))
-
-(define (expand-clauses clauses)
-  (if (null? clauses)
-      'false
-      (let ((first (car clauses))
-            (rest (cdr clauses)))
-        (if (cond-else-clause? first)
-            (if (null? rest)
-                (sequence->exp
-                 (sel:cond-actions first))
-                (error "ELSE clause is not last: COND->IF" clauses))
-            (make-if (sel:cond-predicate first)
-                     (sequence->exp (sel:cond-actions first))
-                     (expand-clauses rest))))))
-
-(define (sequence->exp seq)
-  (cond ((null? seq) seq)
-        ((last-exp? seq) (sel:first-exp seq))
-        (else (make-begin seq))))
-
-(define (make-if predicate consequent alternative)
-  (list 'if predicate consequent alternative))
-
-(define (make-procedure params body env)
-  (list 'procedure params body env))
-
-(define (apply-primitive-proc proc args)
-  (apply-in-guile
-   (sel:primitive-implementation proc) args))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (print object)
   (if (compound-proc? object)
@@ -104,6 +109,7 @@
       (print output)))
   (driver-loop))
 
-;; start
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define the-global-env (env:set-up))
 (driver-loop)
